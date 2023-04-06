a93613688fc92b5eb50fc7bb3e34cff586d12236
Ethan-Omeara <129847922+Ethan-Omeara@users.noreply.github.com>
2023-04-07 11:40:32 +1200

    Main Menu

--- Password Vault.py
+++ Password Vault.py
@@ -2,20 +2,26 @@
 USERNAME = "ethan"
 PASSWORD = "password"
 
+# Whether the program should continue running
 active = True
 
-# Contains the main features and information about them
+# A class to contain the program features
+class feature:
+    def __init__(self, description, function):
+        self.description = description
+        self.run = function
+
 def exit():
+    print("Exiting Program")
+    global active
     active = False
 
 features = [
-    {
-        'description': "Exit the program",
-        'run': exit
-    }
+    feature("Exit Program", exit)
 ]
 
 def check_account():
+    '''Checks if username and password are correct'''
     # Get username and password
     username_input = input("Input Username: ")
     password_input = input("Input Password: ")
@@ -30,14 +36,19 @@ def menu_selection():
     # Explain menu
     print("Select function from the list below:")
 
+    # Print the description and number of every feature
     selectionString = ''
     for i, v in enumerate(features):
-        selectionString += str(i + 1) + ' - ' + v['description'] + '\n'
+        selectionString += str(i + 1) + ' - ' + v.description + '\n'
+    selectionString += "Input Selection: "
     
+    # Get the user's input and ensure it is valid
     selection = input(selectionString)
     while not selection.isdigit() or int(selection) > len(features) or int(selection) == 0:
-        print("Invalid input")
+        print("Invalid input, please input a valid number from above")
         selection = input(selectionString)
+    
+    # Return the feature's index
     return int(selection)-1
     
 # Print program information
@@ -47,8 +58,10 @@ your responses on the command line''')
 
 if check_account():
     while active:
+        # Get player's feature selection
         selection = menu_selection()
-        features[selection]['run']()
+        # Run feature
+        features[selection].run()
 
 else:
     print("Incorrect username or password")
